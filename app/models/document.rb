e9149c135307ea02a82a6d1eba4a63e70fcb920d
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-01-12 17:40:01 +0530

    added has_many through association

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,5 @@
 class Document < ApplicationRecord
+  has_many :employee_document
   has_many :employees, through: :employee_document
   validates :document_name, presence: true, length: { maximum: 20 }
 end 
