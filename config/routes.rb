dbb73ba78812577ea238360361974107caf5ef9a
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-01-14 19:27:30 +0530

    Changed Root route to login page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'home#homepage'
+  # root to: "pages#home"
+  resources :employees
   devise_for :users, controllers: {
     confirmations: 'users/confirmations',
     passwords: 'users/passwords',
@@ -7,6 +8,10 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     unlocks: 'users/unlocks'
   }
+  
+  devise_scope :user do 
+    root to: 'users/sessions#new'
+  end 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
