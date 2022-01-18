2d273e0292e7328a4cf5fc22bb4267cb25a049e2
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-01-18 18:43:01 +0530

    Added Address Controller

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,32 @@
+class AddressesController < ApplicationController
+  def new
+    @address = Address.new 
+  end
+
+  def create
+    @address = Address.create(address_params)
+    if @address.save
+      redirect_to employees_path
+    end 
+  end 
+
+  def edit
+    @address = Address.find(params[:id])
+  end 
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update
+      flash[:notice] = "Address was updated successfully" 
+      redirect_to employees_path
+    end 
+  end
+
+  def index
+    @addresses = Address.all
+  end
+end
+
+def address_params
+  params.require(:address).permit(:category, :flat_no, :area, :street_no, :landmark, :city, :district, :state, :postal_code, :employee_id)
+end
