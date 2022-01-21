67df55bb633b60911ed1ea54bcb98c17b4eabc35
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-01-21 12:43:11 +0530

    Changed Doc Max length

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,5 @@
 class Document < ApplicationRecord
   has_many :employee_document
   has_many :employees, through: :employee_document
-  validates :document_name, presence: true, length: { maximum: 20 }
+  validates :document_name, presence: true, length: { maximum: 50 }
 end 
