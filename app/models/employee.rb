9e5107e79a20d3f77619577f778ea079da0263b5
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-01-21 18:07:35 +0530

    Added association for docs in nested form

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,11 +3,15 @@ class Employee < ApplicationRecord
   has_many :addresses, dependent: :destroy
   accepts_nested_attributes_for :addresses 
   validates_associated :addresses
+
   has_many :contacts, dependent: :destroy
   accepts_nested_attributes_for :contacts
-  validates_associated :contacts 
-  has_many :employee_document
-  has_many :documents, through: :employee_document, dependent: :destroy 
+  validates_associated :contacts
+
+  has_many :employee_documents, dependent: :destroy 
+  accepts_nested_attributes_for :employee_documents
+  has_many :documents, through: :employee_documents
+  
   validates :name, :dob, :experience, :department, :designation, :joining_date, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }, 
             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
