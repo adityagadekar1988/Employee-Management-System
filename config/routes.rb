0cc9a5256df0e7a23fd7a9a95a596fc5b2965c82
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-01-21 18:08:40 +0530

    Added resources for documents

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   resources :employees 
   resources :addresses
   resources :contacts
+  resources :documents
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
