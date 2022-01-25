9bf7082d108dc3554901aae6f36fb05f085e3507
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-01-25 12:50:45 +0530

    Added requirements for Bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery
 //= require jquery_ujs
+//= require popper
+//= require bootstrap
 import "@hotwired/turbo-rails"
 import "controllers"
