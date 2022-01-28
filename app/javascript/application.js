66fe706eff963b5105ad65b2cebd83951d3dff6c
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-01-28 20:11:33 +0530

    Added jquery to add and remove address field

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require jquery
-//= require jquery_ujs
+//=require jquery
+//=require jquery_ujs
 //= require popper
 //= require bootstrap
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$(document).ready(function () {
+  $('#add_address').click(function(evt) {
+    $('div#new_address').append(('#address_form').html() +    
+    '<div><button class="removeAddress", type="button">remove</button></div>');
+  });
+  
+  $('#new_address').on('click', '.removeAddress', function(evt) {
+    $(this).closest('#address_form').remove();
+    console.log("hello");
+  });
+});
