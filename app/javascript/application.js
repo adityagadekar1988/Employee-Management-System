c3a38dcb20866fa7386766589a513d68658fd49b
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-02-03 16:56:06 +0530

    Added JS as per gem

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,28 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//=require jquery
-//=require jquery_ujs
+//= require jquery
+//= require jquery_ujs
 //= require popper
 //= require bootstrap
 //= require nested_form_fields
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+$(document).ready(function () {
+  $('.form')
+    .on("fields_added", function () {console.log ("Hello");
+      if ($(".fields-row").length > 1) {
+        $(".remove_fields")[0].style.display = "block";
+      } else {
+        $(".remove_fields")[0].style.display = "none";
+      }
+    })
+  $('.form')  
+    .on("fields_removed", function () {
+      if ($(".fields-row").length == 1) {
+        $(".remove_fields")[0].style.display = "none";
+      } else {
+        $(".remove_fields")[0].style.display = "block";
+      }
+    });
+});
