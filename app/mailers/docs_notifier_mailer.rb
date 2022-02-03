d63d2bb30834fa447e6e0a416f71130d91b07797
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-02-03 17:01:33 +0530

    Generated mailer

---
+++ app/mailers/docs_notifier_mailer.rb
@@ -0,0 +1,8 @@
+class DocsNotifierMailer < ApplicationMailer
+  default from: 'adityagadekar1988@gmail.com'
+
+  def notification_email
+    @employee = params[:employee]
+    mail(to: 'adityagadekar.ror@gmail.com', subject: 'Documents Missing from <%= @employee.name %>', date: <%= DateTime.now %>)
+  end 
+end
---
+++ test/mailers/previews/docs_notifier_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/docs_notifier_mailer
+class DocsNotifierMailerPreview < ActionMailer::Preview
+
+end
