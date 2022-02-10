0391dbfe1f8fcc5675eb73770887fe54b2754c7d
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-02-10 14:54:21 +0530

    Added all employees in instance variable

--- app/mailers/docs_notifier_mailer.rb
+++ app/mailers/docs_notifier_mailer.rb
@@ -2,7 +2,7 @@ class DocsNotifierMailer < ApplicationMailer
   default from: 'adityagadekar1988@gmail.com'
 
   def notification_email
-    @employee = params[:employee] 
+    @employees = Employee.all 
     #@employee.each do |employee|      
       mail(to: 'adityagadekar.ror@gmail.com', 
            subject: 'Reminder for missing documents')   
