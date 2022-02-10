9861c678cd59e84357d6d4ed5c0a669c12d12623
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-02-10 15:34:41 +0530

    Removed commented line

--- app/mailers/docs_notifier_mailer.rb
+++ app/mailers/docs_notifier_mailer.rb
@@ -1,10 +1,9 @@
 class DocsNotifierMailer < ApplicationMailer
   default from: 'adityagadekar1988@gmail.com'
 
-  def notification_email
-    @employees = Employee.all 
-    #@employee.each do |employee|      
-      mail(to: 'adityagadekar.ror@gmail.com', 
-           subject: 'Reminder for missing documents')   
+  def notification_email 
+    @employees = Employee.all      
+    mail(to: 'adityagadekar.ror@gmail.com', 
+          subject: 'Reminder for missing documents')   
   end 
 end
