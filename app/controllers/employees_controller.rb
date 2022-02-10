2a740416b321da174df610849f13056cb9cf9e90
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-02-10 15:36:26 +0530

    Removed mailer action frm logic

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -11,7 +11,6 @@ class EmployeesController < ApplicationController
     @employee = Employee.new(employee_params)
     @doc = Document.all.count  
     if @employee.save
-      #DocsNotifierMailer.with(employee: @employee).notification_email.deliver_later if employee_params[:document_ids].length < (@doc + 1)
       flash[:notice] = "Employee was added successfully."
       redirect_to employees_path
     else
@@ -31,7 +30,6 @@ class EmployeesController < ApplicationController
     @employee = Employee.find(params[:id]) 
     @doc = Document.all.count 
     if @employee.update(employee_params)
-      #DocsNotifierMailer.with(employee: @employee).notification_email.deliver_now if employee_params[:document_ids].length < (@doc + 1)
       flash[:notice] = "Employee details were updated successfully."
       redirect_to employees_path
     else
