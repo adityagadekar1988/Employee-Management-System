fca277280ecd8f47208172452a7be76e445b3139
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-02-11 16:03:54 +0530

    Removed if statement as not needed

--- app/mailers/docs_notifier_mailer.rb
+++ app/mailers/docs_notifier_mailer.rb
@@ -1,8 +1,8 @@
 class DocsNotifierMailer < ApplicationMailer
   default from: 'adityagadekar1988@gmail.com'
 
-  def notification_email 
-    @employees = Employee.all      
+  def notification_email
+    @employees = Employee.left_outer_joins(:documents).group('employees.id').having("COUNT(documents.id) < #{Document.count}")      
     mail(to: 'adityagadekar.ror@gmail.com', 
           subject: 'Reminder for missing documents')   
   end 
