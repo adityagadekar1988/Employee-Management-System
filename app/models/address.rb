014e74fc4764e94cb7a26504e966d3e5c11d7574
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-02-16 22:57:44 +0530

    Trasferred validations from address to permanent

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,3 @@
 class Address < ApplicationRecord
   belongs_to :employee, inverse_of: :addresses 
-  validates :first_line, :second_line, :city, :district, :state, :type, presence: true 
-  validates :postal_code, presence: true, length: { is: 6 }
 end 
