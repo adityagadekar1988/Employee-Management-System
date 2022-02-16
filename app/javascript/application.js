7f7ea243c63e3f2a5577cbb7364c3c0473c47a27
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-02-16 13:08:20 +0530

    Added script to make both addresses same

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require popper
 //= require bootstrap
@@ -9,21 +10,47 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 
-$(document).ready(function () {
-  $('.nested_form_field').on("fields_added.nested_form_fields", function () { 
-    if ($(".fields-row").length > 1) {
-      $(".remove_fields").show();
-    } else {
-      $(".remove_fields").hide();
+$(document).ready(function(){
+  $("#make_same").on("click", function(){
+    if (this.checked) { 
+      alert(23);
+      $("#employee_local_attributes_first_line").val($("#employee_permanent_attributes_first_line").val());
+      $("#employee_local_attributes_second_line").val($("#employee_permanent_attributes_second_line").val());
+      $("#employee_local_attributes_landmark").val($("#employee_permanent_attributes_landmark").val());
+      $("#employee_local_attributes_city").val($("#employee_permanent_attributes_city").val());
+      $("#employee_local_attributes_district").val($("#employee_permanent_attributes_district").val());
+      $("#employee_local_attributes_state").val($("#employee_permanent_attributes_state").val());
+      $("#employee_local_attributes_postal_code").val($("#employee_permanent_attributes_postal_code").val());                         
     }
-  });
-  
-  $('.nested_form_field').on("fields_removed.nested_form_fields", function () {
-    //alert($(".fields-row").length);
-    if ($(".fields-row").length == 1) {
-      $(".remove_fields").hide();
-    } else {
-      $(".remove_fields").show();
+    else {
+      $("#employee_local_attributes_first_line").val('');
+      $("#employee_local_attributes_second_line").val('');
+      $("#employee_local_attributes_landmark").val('');
+      $("#employee_local_attributes_city").val('');
+      $("#employee_local_attributes_district").val('');
+      $("#employee_local_attributes_state").val('');
+      $("#employee_local_attributes_postal_code").val('');     
     }
   });
 });
+  
+
+
+// $(document).ready(function () {
+//   $('.nested_form_field').on("fields_added.nested_form_fields", function () { 
+//     if ($(".fields-row").length > 1) {
+//       $(".remove_fields").show();
+//     } else {
+//       $(".remove_fields").hide();
+//     }
+//   });
+  
+//   $('.nested_form_field').on("fields_removed.nested_form_fields", function () {
+//     //alert($(".fields-row").length);
+//     if ($(".fields-row").length == 1) {
+//       $(".remove_fields").hide();
+//     } else {
+//       $(".remove_fields").show();
+//     }
+//   });
+// });
