17c671d703756456c8bcab68a5dbdf3e972782dc
adityagadekar1988 <adityagadekar1988@gmail.com>
2022-02-24 16:43:07 +0530

    Added preview for mailer

--- test/mailers/previews/docs_notifier_mailer_preview.rb
+++ test/mailers/previews/docs_notifier_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/docs_notifier_mailer
 class DocsNotifierMailerPreview < ActionMailer::Preview
-
+	def notification_email
+		DocsNotifierMailer.notification_email
+	end
 end
